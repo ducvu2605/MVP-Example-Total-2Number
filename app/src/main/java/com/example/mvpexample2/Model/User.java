3a7c1aa13f538ab7f254ebc17a828517bcb7245e
ndv260599 <nguyenducvu26051999@gmail.com>
2020-07-09 10:07:46 +0700

    - Add Model User

--- app/src/main/java/com/example/mvpexample2/Model/User.java
+++ app/src/main/java/com/example/mvpexample2/Model/User.java
@@ -1,4 +1,30 @@
-package com.example.mvpexample2;
+package com.example.mvpexample2.Model;
 
-public class Model {
+public class User {
+    private String idName;
+    private String passWord;
+
+    public User() {
+    }
+
+    public User(String idName, String passWord) {
+        this.idName = idName;
+        this.passWord = passWord;
+    }
+
+    public String getIdName() {
+        return idName;
+    }
+
+    public void setIdName(String idName) {
+        this.idName = idName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
 }
